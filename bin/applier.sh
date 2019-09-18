f425340c43bab775dc4e4c28c422cb2fb5639847
Ava Shulman <cshulman@redhat.com>
2019-09-18 11:05:59 -0400

    first commit

---
+++ bin/applier.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install galaxy
+ansible-galaxy install -r requirements.yml -p galaxy
+
+# Grab cluster id
+cluster_id=$(oc get machinesets -n openshift-machine-api -o jsonpath='{.items[0].metadata.labels.machine\.openshift\.io\/cluster-api-cluster}')
+
+# Grab cloud region
+cloud_region=$(oc get machinesets -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.placement.region}')
+
+# Run applier
+ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region}
+
+
