3cdaf6c53b0f5ff370430290f2a9fad82be38add
GTPE Student <cshulman95@gmail.com>
2019-10-30 19:32:35 +0000

    added template for creating machinesets

--- bin/applier.sh
+++ bin/applier.sh
@@ -9,7 +9,9 @@ cluster_id=$(oc get machinesets -n openshift-machine-api -o jsonpath='{.items[0]
 # Grab cloud region
 cloud_region=$(oc get machinesets -n openshift-machine-api -o jsonpath='{.items[0].spec.template.spec.providerSpec.value.placement.region}')
 
+#hard coding for the test
+
 # Run applier
-ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region}
+ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region} -e role="infra" -vvv
 
 
