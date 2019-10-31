b12c14bb2983947d6e65ba3cb7677b0c2107e6fd
GTPE Student <cshulman95@gmail.com>
2019-10-31 16:51:21 +0000

    cleanup & variabalized more things

--- bin/applier.sh
+++ bin/applier.sh
@@ -13,8 +13,8 @@ cloud_region=$(oc get machinesets -n openshift-machine-api -o jsonpath='{.items[
 
 # Run applier
 
-#ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region} -e role="ava" -e amiid="ami-0bc59aaa7363b805d" -e include_tags="machinesets" -vvv
+ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region} -e role="infra" -e amiid="ami-0bc59aaa7363b805d" -e machinetype="m5.xlarge" -e include_tags="machinesets" -vvv
 
-ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region} -e role="infra" -e amiid="ami-0bc59aaa7363b805d" -e include_tags="autoscaling" -vvv
+#ansible-playbook -i .applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e clusterid=${cluster_id} -e cloudregion=${cloud_region} -e role="worker" -e amiid="ami-0bc59aaa7363b805d" -e include_tags="autoscaling" -vvv
 
 
